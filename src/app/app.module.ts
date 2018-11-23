9e6b4ed3fc889209b050dcfb0ac0555a1e6d79cf
joaaoeu <hello@joaao.eu>
2018-11-23 14:32:39 -0200

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Components
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { ReportComponent } from './components/report/report.component';
+import { MembersComponent } from './components/members/members.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCardsComponent,
+    ReportComponent,
+    MembersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
