00b57b9c1e2393629b57c894b943a9cd2775e55b
joaaoeu <hello@joaao.eu>
2018-11-23 14:38:01 -0200

    Added basic routes and header UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { ReportComponent } from './components/report/report.component';
+import { MembersComponent } from './components/members/members.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListCardsComponent
+  },
+  {
+    path: 'report',
+    component: ReportComponent
+  },
+  {
+    path: 'members',
+    component: MembersComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
