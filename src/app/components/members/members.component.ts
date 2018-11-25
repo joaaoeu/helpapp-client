bc5d416790c92dc4be2190bb3ca2c46f684d1b6e
joaaoeu <hello@joaao.eu>
2018-11-25 13:56:38 -0200

    Finish members UI and integration

--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -10,10 +10,14 @@ import { MembersService } from '../../services/members/members.service';
 })
 export class MembersComponent implements OnInit {
 
+  requestingMembers = true;
+  requesting = false;
+
   members = [];
   newMemberData = { userType: 3 };
   memberToEditData = {};
   memberToDeleteData = {};
+  errorMessage = '';
   
   constructor(private _membersService: MembersService) { }
 
@@ -22,35 +26,99 @@ export class MembersComponent implements OnInit {
   }
   
   getMembers() {
+    this.requestingMembers = true;
     this._membersService.getMembers()
       .subscribe(
-        res => this.members = res,
+        res => {
+          this.requestingMembers = false;
+          this.members = res;
+        },
         err => console.log(err)
       )
   }
   
   resetNewMemberData() {
-    this.newMemberData = { userType: 3 }; 
+    this.newMemberData = { userType: 3 };
+    this.errorMessage = '';
   }
   
   setMemberToEditData(member) {
-    this.memberToEditData = member;
+    this.memberToEditData = {...member};
+    this.errorMessage = '';
   }
   
   setMemberToDeleteData(member) {
-    this.memberToDeleteData = member;
+    this.memberToDeleteData = {...member};
+    this.errorMessage = '';
   }
   
   createNewMember() {
-    console.log(this.newMemberData);
+    this.requesting = true;
+    
+    if(!this.newMemberData.name || !this.newMemberData.email || !this.newMemberData.password || !this.newMemberData.userType) {
+      this.errorMessage = 'All fields are required!';
+      this.requesting = false;
+      return false;
+    }
+    
+    this._membersService.createNewMember(this.newMemberData)
+    .subscribe(
+      res => {
+        this.members.unshift(res.user);
+        $('#newMemberModal').modal('hide');
+        this.requesting = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.requesting = false;
+      }
+    )
   }
   
   saveMember() {
-    console.log(this.memberToEditData);
+    this.requesting = true;
+    
+    if(!this.memberToEditData.name || !this.memberToEditData.email || !this.memberToEditData.userType) {
+      this.errorMessage = 'All fields, excepts password, are required!';
+      return false;
+    }
+    
+    this._membersService.saveMember(this.memberToEditData)
+    .subscribe(
+      res => {
+        this.members = this.members.map((member) => {
+          if(member._id === this.memberToEditData._id)
+            return res.user;
+          
+          return member;
+        });
+        $('#editMemberModal').modal('hide');
+        this.requesting = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.requesting = false;
+      }
+    )
   }
   
   deleteMember() {
-    console.log(this.memberToDeleteData);
+    this.requesting = true;
+    
+    this._membersService.deleteMember(this.memberToDeleteData)
+    .subscribe(
+      res => {
+        this.members = this.members.filter((member) => {
+          return member._id !== this.memberToDeleteData._id;
+        });
+        $('#confirmDeleteMemberModal').modal('hide');
+        this.requesting = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.requesting = false;
+      }
+    )
   }
 
 }
--- src/app/services/members/members.service.ts
+++ src/app/services/members/members.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { RequestOptions } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -8,9 +9,25 @@ export class MembersService {
 
   private _apiUrl = 'http://localhost:5001/api';
   
+  private _membersUrl = this._apiUrl + '/members';
+  
   constructor(private http: HttpClient) { }
   
   getMembers() {
-    return this.http.get<any>(this._apiUrl + '/members');
+    return this.http.get<any>(this._membersUrl);
+  }
+  
+  createNewMember(member) {
+    return this.http.post<any>(this._membersUrl, member);
+  }
+  
+  saveMember(member) {
+    return this.http.put<any>(this._membersUrl, member);
+  }
+  
+  deleteMember(member) {
+    return this.http.delete<any>(this._membersUrl, new RequestOptions({
+       body: member
+    }));
   }
 }
